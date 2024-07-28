8341da0b7a671d962fa0fa505dfa5e423b4c69ac
Akbar <AkbarHabeeb@users.noreply.github.com>
2024-07-28 21:41:35 +0530

    Add files via upload

---
+++ ptp/lib/helpers/enum.js
@@ -0,0 +1,22 @@
+let Enums = {
+    supportStocks: [
+        "BSE_INDEX|SMLCAP",
+        "NSE_INDEX|NIFTY200 QUALTY30",
+        "NSE_INDEX|NIFTY LARGEMID250",
+        "NSE_INDEX|NIFTY100 Qualty30",
+        "NSE_INDEX|Nifty Media",
+        "BSE_INDEX|POWER",
+        "NSE_INDEX|Nifty Consumption",
+        "BSE_INDEX|FINSER",
+        "NSE_INDEX|Nifty CPSE",
+        "NSE_INDEX|Nifty Midcap 50",
+        "NSE_INDEX|Nifty Auto",
+        "NSE_INDEX|Nifty Realty",
+        "NSE_INDEX|Nifty Multi Mfg"
+    ],
+    responseCode: {
+        Success: 200
+    }
+}
+
+module.exports = Enums;
